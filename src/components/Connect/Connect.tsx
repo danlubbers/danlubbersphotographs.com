7d21ddabd7dd9a5cca4ee98a5d9bc05ab2c93150
Dan Lubbers <lubbers.dan@gmail.com>
2021-08-26 17:00:33 -0400

    Hamburger mobile menu is fixed for different browsers. Animation added to menu as well

--- src/components/Connect/Connect.tsx
+++ src/components/Connect/Connect.tsx
@@ -6,31 +6,27 @@ interface ConnectProps {
   isConnect?: boolean;
 }
 
-const Connect: React.FC<ConnectProps> = ({ isConnect }) => {
-  console.log(isConnect);
-
-  return (
-    <article
-      className={
-        isConnect
-          ? styles.connectContainerActive
-          : styles.connectContainerInactive
-      }
-    >
-      <div className={styles.connectWrapper}>
-        <p className={styles.email}>
-          Email:{` `}
-          <a
-            className={styles.emailLink}
-            href="mailto:lubbers.dan@gmail.com?subject=Mailed from danlubbersphotographs.com"
-          >
-            lubbers.dan@gmail.com
-          </a>
-        </p>
-        <SocialMedia />
-      </div>
-    </article>
-  );
-};
+const Connect: React.FC<ConnectProps> = ({ isConnect }) => (
+  <article
+    className={
+      isConnect
+        ? styles.connectContainerActive
+        : styles.connectContainerInactive
+    }
+  >
+    <div className={styles.connectWrapper}>
+      <p className={styles.email}>
+        Email:{` `}
+        <a
+          className={styles.emailLink}
+          href="mailto:lubbers.dan@gmail.com?subject=Mailed from danlubbersphotographs.com"
+        >
+          lubbers.dan@gmail.com
+        </a>
+      </p>
+      <SocialMedia />
+    </div>
+  </article>
+);
 
 export default Connect;
