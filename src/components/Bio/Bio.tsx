ed57b50de17ac0df878ca58a2a315cedf3defecd
Dan Lubbers <lubbers.dan@gmail.com>
2021-09-06 15:21:55 -0400

    Added more Google events: Arrows, keyboard event, Logo, Bio and Connect areas

--- src/components/Bio/Bio.tsx
+++ src/components/Bio/Bio.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 // import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import bioImage from '../../assets/images/dan-portrait-arcteryx-sv-web-icon.jpg';
 import * as styles from './Bio.module.scss';
-
 import clients from '../../data/clients.json';
 import editorials from '../../data/editorials.json';
 import press from '../../data/press.json';
