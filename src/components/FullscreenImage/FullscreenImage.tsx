68db0876462d42d089a94c01bca9b93399394ecb
Dan Lubbers <lubbers.dan@gmail.com>
2021-09-13 19:19:45 -0400

    Adding Fullscreen functionality

---
+++ src/components/FullscreenImage/FullscreenImage.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { GatsbyImage, IGatsbyImageData } from 'gatsby-plugin-image';
+import * as styles from './FullscreenImage.module.scss';
+import Portal from '../Portal/Portal';
+
+interface FullscreenImageProps {
+  setShowFullscreen: (value: boolean | ((prevVar: boolean) => boolean)) => void;
+  image: IGatsbyImageData;
+  title: string;
+}
+
+const FullscreenImage: React.FC<FullscreenImageProps> = ({
+  setShowFullscreen,
+  image,
+  title,
+}) => (
+  <>
+    <Portal>
+      <div className={styles.fullscreenImageContainer}>
+        <div className={styles.iconWrapper}>
+          <div className={styles.icon} onClick={() => setShowFullscreen(false)}>
+            X
+          </div>
+        </div>
+        <div className={styles.imageWrapper}>
+          <GatsbyImage className={styles.image} image={image} alt={title} />
+        </div>
+      </div>
+    </Portal>
+  </>
+);
+
+export default FullscreenImage;
--- src/components/GallerySlider/GallerySlider.tsx
+++ src/components/GallerySlider/GallerySlider.tsx
@@ -2,11 +2,13 @@ import React, { useState, useCallback, useEffect } from 'react';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 import recordEvent from 'utilities/recordEvents';
+import FullscreenImage from 'components/FullscreenImage/FullscreenImage';
 import * as styles from './GallerySlider.module.scss';
 import CopyrightOverlay from '../CopyrightOverlay/CopyrightOverlay';
 
 function GallerySlider({ sliderData, show }) {
   const [current, setCurrent] = useState(0);
+  const [showFullscreenImage, setShowFullscreenImage] = useState(false);
   const { length } = sliderData;
 
   if (!Array.isArray(sliderData) || sliderData.length <= 0) return null;
@@ -42,29 +44,42 @@ function GallerySlider({ sliderData, show }) {
   const imageSliderData = sliderData.map((data, idx) => {
     const image = getImage(data.src);
     return (
-      <div
-        className={
-          idx === current ? `${styles.slideActive}` : `${styles.slide}`
-        }
-        key={data.id}
-      >
-        {idx === current && (
-          <div className={styles.imageDesktopWrapper}>
-            <div className={styles.imageDescriptionContainer}>
-              <h1 className={styles.imageName}>{data.name}</h1>
-              <h1 className={styles.imageDescription}>{data.description}</h1>
+      <>
+        <div
+          className={
+            idx === current ? `${styles.slideActive}` : `${styles.slide}`
+          }
+          key={data.id}
+        >
+          {idx === current && (
+            <div className={styles.imageDesktopWrapper}>
+              <div className={styles.imageDescriptionContainer}>
+                <h1 className={styles.imageName}>{data.name}</h1>
+                <h1 className={styles.imageDescription}>{data.description}</h1>
+              </div>
+              <GatsbyImage
+                image={image}
+                alt={data.name}
+                onClick={() => setShowFullscreenImage(!showFullscreenImage)}
+              />
             </div>
-            <GatsbyImage image={image} alt={data.name} />
+          )}
+          <div className={styles.imageMobileWrapper}>
+            <GatsbyImage
+              className={styles.mobileImage}
+              image={image}
+              alt={data.name}
+            />
           </div>
-        )}
-        <div className={styles.imageMobileWrapper}>
-          <GatsbyImage
-            className={styles.mobileImage}
-            image={image}
-            alt={data.name}
-          />
         </div>
-      </div>
+        {idx === current && showFullscreenImage && (
+          <FullscreenImage
+            setShowFullscreen={setShowFullscreenImage}
+            image={image}
+            title={data.name}
+          />
+        )}
+      </>
     );
   });
 
