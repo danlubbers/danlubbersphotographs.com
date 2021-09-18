645468e31348d7b9e952e059dba7ffbd069e351c
Dan Lubbers <lubbers.dan@gmail.com>
2021-09-17 21:26:02 -0400

    Fixed a small bug in the recordEvent function

--- src/utilities/recordEvents.tsx
+++ src/utilities/recordEvents.tsx
@@ -1,6 +1,6 @@
 const recordEvent = (category: string, action: string) => {
   if (typeof window !== `undefined`) {
-    window.gtag(`event`, {
+    window.gtag(`event`, action, {
       event_category: category,
       event_action: action,
     });
