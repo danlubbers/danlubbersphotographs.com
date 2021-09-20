5b9accd531575bbfcd0af0c0ea8dbbd87a7fb73a
Dan Lubbers <lubbers.dan@gmail.com>
2021-09-20 16:09:16 -0400

    Took off another accidental www.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   // Since `gatsby-plugin-typescript` is automatically included in Gatsby you
   // don't need to define it here (just if you need to change the options)
   siteMetadata: {
-    siteUrl: `https://www.danlubbersphotographs.com`,
+    siteUrl: `https://danlubbersphotographs.com`,
     title: `Dan Lubbers Photographs`,
     lang: `en`,
     titleTemplate: ``,
